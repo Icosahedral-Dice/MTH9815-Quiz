0e66f4397991f1543cbde24acc859e675e61a17a
Mingsen Wang <mingsen.wang.sh@gmail.com>
2022-11-22 13:16:14 -0500

    Implemented quicksort

---
+++ MTH9815-Quiz1/QuickSort.h
@@ -0,0 +1,48 @@
+//
+//  QuickSort.h
+//  MTH9815-Quiz1
+//
+//  Created by 王明森 on 11/22/22.
+//
+
+#ifndef QuickSort_h
+#define QuickSort_h
+#include <functional>
+
+void Swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int Partition(int* arr, int lo, int hi) {
+    int pivot = arr[hi];    // Last element as pivot
+    
+    int i = lo - 1;
+    
+    for (int j = lo; j < hi; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            Swap(&arr[i], &arr[j]);
+        }
+    }
+    
+    i++;
+    Swap(&arr[i], &arr[hi]);
+    
+    return i;
+}
+
+void QuickSort_impl(int* arr, int lo, int hi) {
+    if (hi - lo > 0 || lo < 0) {
+        int pivot = Partition(arr, lo, hi);
+        QuickSort_impl(arr, lo, pivot - 1);
+        QuickSort_impl(arr, pivot + 1, hi);
+    }
+}
+
+void QuickSort(int* arr, int count) {
+    QuickSort_impl(arr, 0, count - 1);
+}
+
+#endif /* QuickSort_h */
--- MTH9815-Quiz1/main.cpp
+++ MTH9815-Quiz1/main.cpp
@@ -2,13 +2,29 @@
 //  main.cpp
 //  MTH9815-Quiz1
 //
-//  Created by 王明森 on 11/21/22.
+//  Created by 王明森 on 11/22/22.
 //
 
 #include <iostream>
+#include "QuickSort.h"
+
+void PrintArray(const int* arr, std::size_t size) {
+    for (std::size_t i = 0; i < size; i++) {
+        std::cout << arr[i] << '\t';
+    }
+    std::cout << std::endl;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+//    int arr[] = {1, 1, 4, 5, 1, 4};
+    int arr[] = {1, 9, 1, 9, 8, 1, 0};
+    int count = sizeof(arr) / sizeof(arr[0]);
+    PrintArray(arr, count);
+    QuickSort(arr, count);
+    PrintArray(arr, count);
+    
+
+    
     return 0;
 }
